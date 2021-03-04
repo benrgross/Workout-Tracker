6c28d10b4d3ececa46ba2f70dc2049f975130dd8
Ben Gross <benrgross@gmail.com>
2021-03-04 15:38:25 -0500

    workout model.

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.new,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter an exercise type ",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter an exercise name ",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
